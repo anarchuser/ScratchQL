6e16ef7aa3a113de39f30b2df827803ef1ab45a0
Aaron Alef <aaron.alef@code.berlin>
2020-02-07 17:36:33 +0100

    feat(database): add empty Database class, holding a matrix of cells
    a 'cell' is a std::variant <bool, long, double, std::string>

--- src/config.h
+++ src/config.h
@@ -1,9 +1,13 @@
 #ifndef DATABASE_CONFIG_H
 #define DATABASE_CONFIG_H
 
+#include <string>
 #include <cstdlib>
+#include <variant>
 
 static const std::string PROJECT_ROOT (std::getenv("SCRATCHQL_ROOT"));
+
+using cell = std::variant<bool, long, double, std::string>;
 
 #endif //DATABASE_CONFIG_H
 
--- test/Model/Database/Database.cpp
+++ test/Model/Database/Database.cpp
@@ -1,2 +1,4 @@
+#include "../../main.h"
+#include "../../../src/Model/Database/Database.h"
 
 /* Copyright (C) 2020 Aaron Alef & Felix Bachstein */
