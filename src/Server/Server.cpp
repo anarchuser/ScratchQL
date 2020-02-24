540beec03b91ccdcbd9212821f70eb304ce3637b
Aaron Alef <aaron.alef@code.berlin>
2020-02-24 11:56:30 +0100

    feat: add empty classes with their relation
    Now the Server communicates over the Parser with the DBMS.
    The Parser acts as bridge in-between.

---
+++ src/Server/Server.cpp
@@ -0,0 +1,9 @@
+#include "Server.h"
+
+template <class T>
+Server <T>::Server (T object) : object{object} {}
+
+template <class T>
+void Server <T>::listen (unsigned long IP, unsigned int port) {}
+
+/* Copyright (C) 2020 Aaron Alef & Felix Bachstein */
