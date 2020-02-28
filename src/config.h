d527443b004f0a16633f7df05c80262fc5878f2a
Aaron Alef <aaron.alef@code.berlin>
2020-02-28 15:18:02 +0100

    refactor: replace duplicate {Log; throw} calls with a macro (#22)

--- src/config.h
+++ src/config.h
@@ -1,12 +1,23 @@
 #ifndef DATABASE_CONFIG_H
 #define DATABASE_CONFIG_H
 
+/// Minimum LOG level. 0 = Everything, 1 = Ignore LOG (INFO), ...
+#define GOOGLE_STRIP_LOG 0
+
+#define THROW(error)                \
+    LOG (ERROR) << error.what();    \
+    throw error;
+
 #include <string>
 #include <cstdlib>
 #include <variant>
 #include <glog/logging.h>
+#include <netinet/in.h>
 
 #include "DBMS/Cell/Cell.h"
+
+#define IP_ADDRESS_FAMILY AF_INET
+#define ADDRESS "localhost:50051"
 
 static const std::string PROJECT_ROOT (std::getenv("SCRATCHQL_ROOT"));
 
