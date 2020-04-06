0de05296eb2414f590a1950d6c3c2d328fcc6d8c
Aaron Alef <aaron.alef@code.berlin>
2020-04-06 15:36:29 +0200

    style(log): remove glog error messages for tests (#29)
    It replaces all LOG (ERROR)s with LOG (WARNING)s

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 export GLOG_stderrtreshold="$1"
 export GLOG_minloglevel="$1"
 export SCRATCHQL_ROOT=$PWD
-cmake --build cmake-build-debug --target Test > /dev/zero #2> /dev/zero
+cmake --build cmake-build-debug --target Test
 result=$?
 if test $result -ne 0
 then echo "Building failed!"
