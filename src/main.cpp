c739d3272a30cf0743448616f06ad68b465155fa
Aaron Alef <aaron.alef@code.berlin>
2020-04-25 12:58:22 +0200

    chore(ci): update Cap'n Proto to v0.8.0 (and refactor main...)

--- src/main.cpp
+++ src/main.cpp
@@ -25,70 +25,32 @@ int main (int argc, char * argv[]) {
     LOG (INFO) << PROJECT_ROOT;
 
     std::string address (argc > 2 ? argv[2] : STR+ "*:" + std::to_string (PORT));
-    auto execServer = [] (std::string const & address, bool never_done = false) -> capnp::EzRpcServer const & {
+    auto execServer = [] (std::string const & address, bool never_done = false) -> kj::Own <capnp::EzRpcServer> {
         (std::cout << "Setting database up on '" << address << "'...").flush();
-        capnp::EzRpcServer server (kj::heap <DatabaseImpl <DBMS>> (), address);
-        server.getPort().wait (server.getWaitScope());
+        kj::Own <capnp::EzRpcServer> server = kj::heap <capnp::EzRpcServer> (kj::heap <DatabaseImpl <DBMS>> (), address);
+        server->getPort().wait (server->getWaitScope());
         std::cout << " Done" << std::endl;
 
-        if (never_done) kj::NEVER_DONE.wait (server.getWaitScope ());
-        return std::move (server);
+        if (never_done) kj::NEVER_DONE.wait (server->getWaitScope ());
+        return server;
     };
 
-    //TODO: move client implementation back to client class which currently refuses to connect
     auto execClient = [] (std::string const & address, int port = -1) {
-        (std::cout << "Connecting to server on '" << address << "'...").flush();
-
-        auto ezClient = kj::heap <capnp::EzRpcClient> (address, port);
-        auto client   = kj::heap <RPCServer::Client> (ezClient->getMain <RPCServer>());
-        auto & waitScope = ezClient->getWaitScope();
-        client->connectRequest().send().wait (waitScope);
-        
-        std::cout << " Done" << std::endl;
-
-        auto sendQuery = [&](std::string const & query) -> Response {
-            auto request = client->sendQueryRequest ();
-            request.setQuery (query);
-            return Wrapper::unwrapResponse (request.send().wait (waitScope).getResponse());
-        };
-
-        std::string query;
-        while (true) {
-            (std::cout << "$ ").flush();
-
-            // Request input
-            getline (std::cin, query);
-            bool pass = false;
-            for (char c : query) if (c != ' ' && c != '\n') pass = true;
-            if (!pass || query.empty()) continue;
-
-            if (query == "exit") return;
-            if (query == "ping") client->connectRequest().send().wait (waitScope);
-            else {
-                std::cout << query << std::endl;
-                try {
-                    std::cout << sendQuery (query);
-                } catch (std::exception & e) {
-                    std::cout << "Server: '" << e.what() << "'" << std::endl;
-                }
-            }
+        try {
+            Client(address, port).startInterface ([] (Response r) {std::cout << r;});
+        } catch (kj::Exception & e) {
+            std::cerr << e.getDescription() << " (" << address << ")" << std::endl;
+//            throw;
         }
-
-//        try {
-//            client.startInterface ([] (Response r) {std::cout << r;});
-//        } catch (kj::Exception & e) {
-//            std::cerr << e.getDescription() << " (" << address << ")" << std::endl;
-////            throw;
-//        }
     };
 
     if (argc == 1) {
-        auto const & server = execServer (address);
-        execClient (ADDRESS, PORT);
+        kj::Own <capnp::EzRpcServer> server = execServer (address);
+        execClient (ADDRESS, server->getPort().wait(server->getWaitScope()));
     } else {
         std::string param (argv [1]);
         if (param == "--server") {
-            auto const & server = execServer (address, true);
+            kj::Own <capnp::EzRpcServer> server = execServer (address, true);
         }
         else if (param == "--client") {
             if (argc > 2) {
